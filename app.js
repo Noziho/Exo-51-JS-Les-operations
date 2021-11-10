87706590d9f0baec3a266d6d8fa5e3d4654ad4af
Noah2211 <noah.decroix3@gmail.com>
2021-11-10 11:53:16 +0100

    FUll exo

---
+++ app.js
@@ -0,0 +1,22 @@
+let calcul = 5 + 6;
+
+let result;
+result = calcul;
+
+console.log(result)
+
+let addition = result + 2;
+
+console.log(addition);
+
+let soustraction = result - 1;
+
+console.log(soustraction)
+
+alert(result * addition)
+
+alert(result ** soustraction)
+
+let modulo = 5 % 2;
+
+console.log(modulo);
